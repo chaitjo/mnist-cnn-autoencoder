5f31731c5cd4ee1452fd31bbca460d0c9f6e9a31
Matouš Skála <skala.matous@gmail.com>
2016-10-23 19:19:49 +0800

    load our dataset to tflearn

--- Part1-cnn/cnn.py
+++ Part1-cnn/cnn.py
@@ -1,26 +1,41 @@
 import numpy as np
 import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d
 from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
+import tensorflow as tf
 
-# Data loading and preprocessing
-import tflearn.datasets.mnist as mnist
-X, Y, testX, testY = mnist.load_data(one_hot=True)
-X = X.reshape([-1, 28, 28, 1])
-testX = testX.reshape([-1, 28, 28, 1])
-# ^ We need to change this!!
+X, Y = tflearn.data_utils.image_preloader('../Data/Train', image_shape=(28, 28),
+	mode='folder', categorical_labels=True)
+testX, testY = tflearn.data_utils.image_preloader('../Data/Test', image_shape=(28, 28),
+	mode='folder', categorical_labels=True)
+
+class ReshapedImagePreloader(object):
+	def __init__(self, preloader):
+		self.preloader = preloader
+
+	def __getitem__(self, id):
+		img = self.preloader[id]
+		img = np.array(img)
+		img = img.reshape([-1, 28, 28, 1])
+		return img
+
+	def __len__(self):
+		return len(self.preloader)
+
+X = ReshapedImagePreloader(X)
+testX = ReshapedImagePreloader(testX)
 
 # Building convolutional network
 network = input_data(shape=[None, 28, 28, 1], name='input')
-network = conv_2d(network, 20, 9, 
-				strides=1 , padding='same' , activation='relu', 
-				bias=True, weights_init='truncated_normal', 
+network = conv_2d(network, 20, 9,
+				strides=1, padding='same', activation='relu',
+				bias=True, weights_init='truncated_normal',
 				regularizer='L2', weight_decay=0.001)
 network = avg_pool_2d(network, 2)
-network = fully_connected(network, 10, activation='softmax', 
-						bias=True, weights_init='truncated_normal', bias_init='zeroes',
+network = fully_connected(network, 10, activation='softmax',
+						bias=True, weights_init='truncated_normal', bias_init='zeros',
 						regularizer='L2', weight_decay=0.001)
 network = regression(network, optimizer='adam', learning_rate=0.01,
                      loss='categorical_crossentropy', name='target')
