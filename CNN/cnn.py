ce7f3fe23d0e48d85daba02e2d40b01364e94bcf
chaitjo <ckjoshi9@gmail.com>
2016-10-25 01:02:27 +0800

    added flags to cnn.py

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -4,7 +4,14 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d
 from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
+from tflearn.layers.optimizers import Momentum
 import tensorflow as tf
+import os
+
+tf.flags.DEFINE_string("learning_rate", 0.01)
+tf.flags.DEFINE_string("momentum", 0.9)
+tf.flags.DEFINE_string("lr_decay", 0.96)
+tf.flags.DEFINE_string("batch_size", 50)
 
 X, Y = tflearn.data_utils.image_preloader('../Data/Train', image_shape=(28, 28),
 										mode='folder', categorical_labels=True)
@@ -38,19 +45,22 @@ network = fully_connected(network, 10, activation='softmax',
 						bias=True, weights_init='truncated_normal', bias_init='zeros',
 						regularizer='L2', weight_decay=0.001)
 
-sgd = tflearn.optimizers.SGD (learning_rate=0.01, 
-							lr_decay=0.96, decay_step=100)
-network = regression(network, optimizer=sgd, learning_rate=0.01,
+sgd_momentum = Momentum(learning_rate=learning_rate, momentum=momentum, 
+						lr_decay=lr_decay, decay_step=100)
+network = regression(network, optimizer=sgd_momentum, learning_rate=learning_rate,
 					loss='categorical_crossentropy', metric='accuracy', name='target')
 
 # Training
-model_name = '1layer_sgd_lr0.01_dec0.96_batch50'
+model_name = 'sgdm_1layer_lr{}_mom{}_dec{}_batch{}'.format(learning_rate, momentum, lr_decay, batch_size)
+os.makedirs('/checkpoints/'+model_name)
+os.makedirs('/best_checkpoints/'+model_name)
 
 model = tflearn.DNN(network, 
-					tensorboard_verbose=0, tensorboard_dir='summaries', checkpoint_path=None, 
-					best_checkpoint_path='best_checkpoints/'+model_name+'/best_checkpoints', best_val_accuracy=0.95)
+					tensorboard_verbose=0, tensorboard_dir='summaries', 
+					checkpoint_path='checkpoints/'+model_name+'/checkpoints', 
+					best_checkpoint_path='best_checkpoints/'+model_name+'/best_checkpoints', best_val_accuracy=0.85)
 model.fit({'input': X}, {'target': Y}, n_epoch=25,
 		validation_set=({'input': testX}, {'target': testY}),
-		batch_size=50, shuffle=True,
+		batch_size=batch_size, shuffle=True,
 		snapshot_step=50, snapshot_epoch=True,
 		show_metric=True, run_id=model_name)
