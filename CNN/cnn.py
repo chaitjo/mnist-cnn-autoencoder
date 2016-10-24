c9395d456571515a819c83fda7e3b01ca15982cd
chaitjo <ckjoshi9@gmail.com>
2016-10-24 16:44:21 +0800

    summaries of initial cnn tests

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -7,9 +7,9 @@ from tflearn.layers.estimator import regression
 import tensorflow as tf
 
 X, Y = tflearn.data_utils.image_preloader('../Data/Train', image_shape=(28, 28),
-	mode='folder', categorical_labels=True)
+										mode='folder', categorical_labels=True)
 testX, testY = tflearn.data_utils.image_preloader('../Data/Test', image_shape=(28, 28),
-	mode='folder', categorical_labels=True)
+										mode='folder', categorical_labels=True)
 
 class ReshapedImagePreloader(object):
 	def __init__(self, preloader):
@@ -37,11 +37,20 @@ network = avg_pool_2d(network, 2)
 network = fully_connected(network, 10, activation='softmax',
 						bias=True, weights_init='truncated_normal', bias_init='zeros',
 						regularizer='L2', weight_decay=0.001)
-network = regression(network, optimizer='adam', learning_rate=0.01,
-                     loss='categorical_crossentropy', name='target')
+
+sgd = tflearn.optimizers.SGD (learning_rate=0.01, 
+							lr_decay=0.96, decay_step=100)
+network = regression(network, optimizer=sgd, learning_rate=0.01,
+					loss='categorical_crossentropy', metric='accuracy', name='target')
 
 # Training
-model = tflearn.DNN(network, tensorboard_verbose=0)
+model_name = '1layer_sgd_lr0.01_dec0.96_batch50'
+
+model = tflearn.DNN(network, 
+					tensorboard_verbose=0, tensorboard_dir='summaries', checkpoint_path=None, 
+					best_checkpoint_path='best_checkpoints/'+model_name+'/best_checkpoints', best_val_accuracy=0.95)
 model.fit({'input': X}, {'target': Y}, n_epoch=25,
-           validation_set=({'input': testX}, {'target': testY}),
-           snapshot_step=100, show_metric=True, run_id='convnet_mnist')
+		validation_set=({'input': testX}, {'target': testY}),
+		batch_size=50, shuffle=True,
+		snapshot_step=50, snapshot_epoch=True,
+		show_metric=True, run_id=model_name)
