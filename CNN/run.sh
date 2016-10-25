e8939b0a2d6b7fc2a954ec5725f8ca289200f4c0
chaitjo <ckjoshi9@gmail.com>
2016-10-26 01:46:57 +0800

    updated run.sh

--- CNN/run.sh
+++ CNN/run.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 source ~/tensorflow_py27_cpu/bin/activate
 cd ~/nn-project-2/CNN/
-python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.95 --batch_size=50
-python cnn.py --learning_rate=0.02 --momentum=0.9 --lr_decay=0.95 --batch_size=50
-python cnn.py --learning_rate=0.05 --momentum=0.9 --lr_decay=0.95 --batch_size=50
-python cnn.py --learning_rate=0.1 --momentum=0.9 --lr_decay=0.95 --batch_size=50
-python cnn.py --learning_rate=0.01 --momentum=0.5 --lr_decay=0.95 --batch_size=50
-python cnn.py --learning_rate=0.01 --momentum=0.75 --lr_decay=0.95 --batch_size=50
-python cnn.py --learning_rate=0.01 --momentum=0.95 --lr_decay=0.95 --batch_size=50
-python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.9 --batch_size=50
-python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.75 --batch_size=50
-python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.5 --batch_size=50
-python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.95 --batch_size=100
-python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.95 --batch_size=75
-python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.95 --batch_size=25
+python cnn.py --learning_rate=0.15 --momentum=0.9 --lr_decay=0.95 --batch_size=50
+python cnn.py --learning_rate=0.2 --momentum=0.9 --lr_decay=0.95 --batch_size=50
+python cnn.py --learning_rate=0.01 --momentum=0.93 --lr_decay=0.95 --batch_size=50
+python cnn.py --learning_rate=0.01 --momentum=0.97 --lr_decay=0.95 --batch_size=50
+python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.93 --batch_size=50
+python cnn.py --learning_rate=0.01 --momentum=0.9 --lr_decay=0.97 --batch_size=50
