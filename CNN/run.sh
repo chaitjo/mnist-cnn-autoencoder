5e0e7058b3065899a22c9269337ce3a9ee4e629e
chaitjo <ckjoshi9@gmail.com>
2016-10-29 18:15:01 +0800

    more runs

--- CNN/run.sh
+++ CNN/run.sh
@@ -3,9 +3,6 @@ source ~/tensorflow_py27_cpu/bin/activate
 cd ~/nn-project-2/CNN/
 python cnn_multilayer.py --filters_hidden1=25 --filters_hidden2=65
 python cnn_multilayer.py --filters_hidden1=35 --filters_hidden2=65
-python cnn_multilayer.py --filters_hidden1=40 --filters_hidden2=65
-python cnn_multilayer.py --filters_hidden1=45 --filters_hidden2=65
-
 
 
 
